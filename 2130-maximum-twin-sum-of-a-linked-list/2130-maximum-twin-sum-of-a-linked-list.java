d3f219ea82253fef2e5584a4bc16a3282d282747
Ishwar749 <77066652+Ishwar749@users.noreply.github.com>
2026-06-14 12:15:09 +0530

    Time: 5 ms (58.5%), Space: 101.2 MB (57.49%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -10,28 +10,37 @@
  */
 class Solution {
     public int pairSum(ListNode head) {
-        Stack<Integer> vals = new Stack<>();
-
-        ListNode dummy = new ListNode();
-        dummy.next = head;
+        ListNode dummy = new ListNode(-1, head);
         ListNode fast = dummy;
         ListNode slow = dummy;
 
         while(fast.next != null && fast.next.next != null) {
             fast = fast.next.next;
             slow = slow.next;
-            vals.push(slow.val);
         }
-        
-        slow = slow.next;
+
+        ListNode reversed = reverseList(slow.next);
 
         int maxSum = 0;
-
-        while(slow != null) {
-            maxSum = Math.max(maxSum, vals.pop() + slow.val);
-            slow = slow.next;            
+        while(reversed != null) {
+            maxSum = Math.max(maxSum, reversed.val + head.val);
+            head = head.next;
+            reversed = reversed.next;
         }
 
         return maxSum;
+    }
+
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+
+        while(head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+
+        return prev;        
     }
 }
