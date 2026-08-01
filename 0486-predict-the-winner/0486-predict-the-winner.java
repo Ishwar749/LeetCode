e89450288503cf992eb9dc2d384113b6881d9e85
Ishwar749 <77066652+Ishwar749@users.noreply.github.com>
2026-08-01 19:19:18 +0530

    Time: 1 ms (51.87%), Space: 43.2 MB (5.44%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,25 +1,36 @@
 class Solution {
     public boolean predictTheWinner(int[] nums) {
         int total = 0;
+        int[][][] dp = new int[nums.length][nums.length][2];
+
+        for(int[][] grid: dp) {
+            for(int[] row: grid) {
+                Arrays.fill(row, -1);
+            }
+        }
+
+
         for(int e: nums) total += e;
 
-        int score = findMaxScore(0, nums.length - 1, 1, nums);
+        int score = findMaxScore(0, nums.length - 1, 1, nums, dp);
         int score2 = total - score;
 
         if(score >= score2) return true;
         return false;
     }
 
-    private int findMaxScore(int i, int j, int turn, int[] nums) {
+    private int findMaxScore(int i, int j, int turn, int[] nums, int[][][] dp) {
         if(i > j) {
             return 0;
         }
 
+        if(dp[i][j][turn] != -1) return dp[i][j][turn];
+
         if(turn == 1) {
-            return Math.max(findMaxScore(i + 1, j, 0, nums) + nums[i], findMaxScore(i, j - 1, 0, nums) + nums[j]);
+            return dp[i][j][turn] = Math.max(findMaxScore(i + 1, j, 0, nums, dp) + nums[i], findMaxScore(i, j - 1, 0, nums, dp) + nums[j]);
         }
         else {
-            return Math.min(findMaxScore(i + 1, j, 1, nums), findMaxScore(i, j - 1, 1, nums));
+            return dp[i][j][turn] = Math.min(findMaxScore(i + 1, j, 1, nums, dp), findMaxScore(i, j - 1, 1, nums, dp));
         }
     }
 }
