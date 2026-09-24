eefcf1a611e498d37c9915e6cf648b88706e2d0b
Ishwar749 <77066652+Ishwar749@users.noreply.github.com>
2026-09-24 23:55:54 +0530

    Time: 184 ms (9.3%), Space: 53.3 MB (5.79%) - LeetHub

---
+++ 0729-my-calendar-i/0729-my-calendar-i.java
@@ -0,0 +1,36 @@
+class MyCalendar {
+
+    private TreeMap<Integer, Integer> map;
+
+    public MyCalendar() {
+        map = new TreeMap<>();    
+    }
+    
+    public boolean book(int startTime, int endTime) {
+        Integer low = map.floorKey(startTime);
+
+        if (low != null && isOverlapping(low, map.get(low), startTime, endTime)) {
+            return false;
+        }
+
+        Integer high = map.ceilingKey(startTime);
+        if (high != null && isOverlapping(startTime, endTime, high, map.get(high))) {
+            return false;
+        }
+
+        map.put(startTime, endTime);
+        return true;
+    }
+
+    private boolean isOverlapping(int a, int b, int c, int d) {
+        System.out.println(a +" " + b +" " + c +" " + d);
+        if (a < c && b <= c) return false;
+        return true;
+    }
+}
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar obj = new MyCalendar();
+ * boolean param_1 = obj.book(startTime,endTime);
+ */
